5e144f26244c2ab93a96606915b124429d29df35
mgencur@redhat.com <mgencur@redhat.com>
2010-09-16 07:43:17 +0200

    ftest for numbergue in cluster environment

--- examples/jsf/numberguess/src/ftest/java/org/jboss/weld/examples/numberguess/clustertest/selenium/NumberGuessClusteringTest.java
+++ examples/jsf/numberguess/src/ftest/java/org/jboss/weld/examples/numberguess/clustertest/selenium/NumberGuessClusteringTest.java
@@ -31,43 +31,39 @@ import org.jboss.test.selenium.locator.XpathLocator;
 import org.jboss.test.selenium.AbstractTestCase;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import static org.jboss.test.selenium.locator.LocatorFactory.*;
-import static org.jboss.test.selenium.guard.request.RequestTypeGuardFactory.*;
 import org.jboss.test.selenium.encapsulated.JavaScript;
-import org.jboss.test.selenium.locator.Attribute;
-import org.jboss.test.selenium.locator.AttributeLocator;
-import org.jboss.test.selenium.locator.XpathLocator;
 import org.jboss.test.selenium.locator.IdLocator;
-import org.jboss.test.selenium.framework.AjaxSelenium;
 import org.jboss.test.selenium.framework.AjaxSeleniumImpl;
 import org.jboss.test.selenium.framework.AjaxSeleniumProxy;
 import org.jboss.test.selenium.guard.request.RequestTypeGuardFactory;
 import org.jboss.test.selenium.locator.ElementLocationStrategy;
-
 import static org.jboss.test.selenium.encapsulated.JavaScript.fromResource;
 import static org.jboss.test.selenium.utils.URLUtils.buildUrl;
 import static org.jboss.test.selenium.SystemProperties.*;
 
 /**
- * This class tests Weld numberguess example in a cluster. Two instances of JBoss AS are
- * being used. First part of test is executed at first (master) instance. Then the first 
- * instance is killed and a second (slave) instance takes over executing of the application.
- * This behaviour simulates recovery from breakdown and session replication.
+ * This class tests Weld numberguess example in a cluster. Two instances of
+ * JBoss AS are being used. First part of test is executed at first (master)
+ * instance. Then the first instance is killed and a second (slave) instance
+ * takes over executing of the application. This behaviour simulates recovery
+ * from breakdown and session replication.
  * 
- * The first version of application server that can be used is JBoss 6.0.0.M1, nevertheless this
- * version of AS has to be updated with current version of Weld core.
+ * The first version of application server that can be used is JBoss 6.0.0.M1,
+ * nevertheless this version of AS has to be updated with current version of
+ * Weld core.
  * 
- * Prior to executing this test it is needed to start both JBoss AS instances manually. 
- * For example (assuming you have created second "all" configuration ("all2")):
- * ./run.sh -c all -g DocsPartition -u 239.255.101.101 -b localhost -Djboss.messaging.ServerPeerID=1 
- * -Djboss.service.binding.set=ports-default
- * ./run.sh -c all2 -g DocsPartition -u 239.255.101.101 -b localhost -Djboss.messaging.ServerPeerID=2
- * -Djboss.service.binding.set=ports-01
- * The configuration all is considered to be master jboss instance (related to 
- * jboss.service.binding.set=ports-default) and the application is deployed to farm directory under
- * chosen jboss configuration directory specified with jboss.master.configuration property.
- *
+ * Prior to executing this test it is needed to start both JBoss AS instances
+ * manually. For example (assuming you have created second "all" configuration
+ * ("all2")): ./run.sh -c all -g DocsPartition -u 239.255.101.101 -b localhost
+ * -Djboss.messaging.ServerPeerID=1 -Djboss.service.binding.set=ports-default
+ * ./run.sh -c all2 -g DocsPartition -u 239.255.101.101 -b localhost
+ * -Djboss.messaging.ServerPeerID=2 -Djboss.service.binding.set=ports-01 The
+ * configuration all is considered to be master jboss instance (related to
+ * jboss.service.binding.set=ports-default) and the application is deployed to
+ * farm directory under chosen jboss configuration directory specified with
+ * jboss.master.configuration property.
+ * 
  * 
  * @author mgencur
  * @author kpiwko
@@ -75,36 +71,26 @@ import static org.jboss.test.selenium.SystemProperties.*;
  */
 public class NumberGuessClusteringTest extends AbstractTestCase
 {
-   protected String MAIN_PAGE = "/home.jsf";
-   
+   protected String MAIN_PAGE = "home.jsf";
+
    protected IdLocator GUESS_MESSAGES = id("numberGuess:messages");
    protected XpathLocator GUESS_STATUS = xp("//div[contains(text(),'I'm thinking of ')]");
-
    protected IdLocator GUESS_FIELD = id("numberGuess:inputGuess");
    protected XpathLocator GUESS_FIELD_WITH_VALUE = xp("//input[@id='numberGuess:inputGuess'][@value=3]");
-   
    protected IdLocator GUESS_SUBMIT = id("numberGuess:guessButton");
    protected IdLocator GUESS_RESTART = id("numberGuess:restartButton");
    protected IdLocator GUESS_SMALLEST = id("numberGuess:smallest");
    protected IdLocator GUESS_BIGGEST = id("numberGuess:biggest");
-
    protected String WIN_MSG = "Correct!";
    protected String LOSE_MSG = "No guesses left!";
    protected String HIGHER_MSG = "Higher!";
-   
    private final String SECOND_INSTANCE_BROWSER_URL = "http://localhost:8180";
    private final long JBOSS_SHUTDOWN_TIMEOUT = 20000;
-   
-   // String jboss = System.getProperty("JBOSS_HOME");
-   {
-      System.out.println("[DEBUG] " + System.getProperty("JBOSS_HOME"));
-   }
-   protected String jboss = "/home/plenyi/dev/server/jboss/jboss-6.0.0-SNAPSHOT-pmuir/";
-   
+   String jbossConfig = System.getProperty("jboss.config");
    private String localContextPath = "";
 
    @BeforeMethod
-   public void openStartURL() throws MalformedURLException 
+   public void openStartURL() throws MalformedURLException
    {
       selenium.open(new URL(contextPath.toString() + MAIN_PAGE));
    }
@@ -113,72 +99,80 @@ public class NumberGuessClusteringTest extends AbstractTestCase
    public void guessingWithFailoverTest() throws MalformedURLException
    {
       preFailurePart();
-	   
+
       String newAddress = getAddressForSecondInstance();
-      
-      /* stop and start browser -> simulate different web browser with different session */
+
+      /*
+       * stop and start browser -> simulate different web browser with different
+       * session
+       */
       super.finalizeBrowser();
       shutdownMasterJBossInstance();
       initializeSecondBrowser();
-      try {
+      super.initializeWaitTimeouts();
+      try
+      {
          super.initializeExtensions();
       }
-      catch (IOException ex) {
-         throw new RuntimeException(ex);
+      catch (IOException e)
+      {
+         new RuntimeException(e.getCause());
       }
-      
-      System.out.println("[DEBUG] " + SECOND_INSTANCE_BROWSER_URL + newAddress);
-      
+
       selenium.open(new URL(SECOND_INSTANCE_BROWSER_URL + newAddress));
 
       assertTrue(selenium.isTextPresent(HIGHER_MSG), "Page should contain message Higher!");
-      assertEquals(Integer.parseInt(selenium.getText(GUESS_SMALLEST)),4, "Page should contain smallest number equal to 4");
-      assertEquals(Integer.parseInt(selenium.getText(GUESS_BIGGEST)),100, "Page should contain biggest number equal to 100");
+      assertEquals(Integer.parseInt(selenium.getText(GUESS_SMALLEST)), 4, "Page should contain smallest number equal to 4");
+      assertEquals(Integer.parseInt(selenium.getText(GUESS_BIGGEST)), 100, "Page should contain biggest number equal to 100");
       assertTrue(selenium.isElementPresent(GUESS_FIELD_WITH_VALUE), "Page should contain input field with value of 3");
-       
-      postFailurePart();       
-       
-      assertTrue(isOnWinPage(), "Win page expected after playing smart.");    
+
+      postFailurePart();
+
+      assertTrue(isOnWinPage(), "Win page expected after playing smart.");
    }
-   
+
    protected void preFailurePart()
    {
-	   int numberOfGuesses = 3;
-	   int guess = 0;
-	   
-	   //enter several guesses (3)
-	   while (true){
-		   while (isOnGuessPage() && guess < numberOfGuesses)
-		   {
-			   enterGuess(++guess);
-		   }
-		   
-		   //we always want to enter at least 3 guesses so that we can continue in the other browser window with expected results
-		   if (guess < numberOfGuesses)
-		   {
-			   resetForm();
-			   guess = 0;
-		   }
-		   else
-		   {
-			   break;
-		   }			   
-	   }
-   }
-   
-   protected void postFailurePart()
-   {	   
-	   int min, max, guess;
-	   int i = 0;
-	   
-	   selenium.deleteAllVisibleCookies();
-	   
-	   while (isOnGuessPage())
+      int numberOfGuesses = 3;
+      int guess = 0;
+
+      // enter several guesses (3)
+      while (true)
       {
-		/*3+8 = 11  -> even though we have 10 attempts, it is possible to enter value 11 times, but
-		 the 11th time it is actually not guessing but only validating that 10 times has gone and the game
-		 is finished (no 11th guessing)*/
-      if (i >= 8)
+         while (isOnGuessPage() && guess < numberOfGuesses)
+         {
+            enterGuess(++guess);
+         }
+
+         // we always want to enter at least 3 guesses so that we can continue
+         // in the other browser window with expected results
+         if (guess < numberOfGuesses)
+         {
+            resetForm();
+            guess = 0;
+         }
+         else
+         {
+            break;
+         }
+      }
+   }
+
+   protected void postFailurePart()
+   {
+      int min, max, guess;
+      int i = 0;
+
+      while (isOnGuessPage())
+      {
+         selenium.deleteAllVisibleCookies();
+         /*
+          * 3+8 = 11 -> even though we have 10 attempts, it is possible to enter
+          * value 11 times, but the 11th time it is actually not guessing but
+          * only validating that 10 times has gone and the game is finished (no
+          * 11th guessing)
+          */
+         if (i >= 8)
          {
             fail("Game should not be longer than 7 guesses in the second selenium after failover");
          }
@@ -193,12 +187,12 @@ public class NumberGuessClusteringTest extends AbstractTestCase
          i++;
       }
    }
-   
+
    protected void resetForm()
    {
       RequestTypeGuardFactory.waitHttp(selenium).click(GUESS_RESTART);
    }
-   
+
    protected void enterGuess(int guess)
    {
       selenium.type(GUESS_FIELD, String.valueOf(guess));
@@ -220,43 +214,43 @@ public class NumberGuessClusteringTest extends AbstractTestCase
    {
       String text = selenium.getText(GUESS_MESSAGES);
       return LOSE_MSG.equals(text);
-   }   
-   
+   }
+
    public String getAddressForSecondInstance()
    {
       String loc = selenium.getLocation().toString();
       String[] parsedStrings = loc.split("/");
       localContextPath = "/" + parsedStrings[3] + "/";
       StringBuilder sb = new StringBuilder();
-      for (int i = 3; i < parsedStrings.length; i++){
+      for (int i = 3; i < parsedStrings.length; i++)
+      {
          sb.append("/").append(parsedStrings[i]);
       }
-      
+
       String newAddress = sb.toString();
       String firstPart = "";
       String sid = "";
-      
+
       if (selenium.isCookiePresent("JSESSIONID"))
       {
          firstPart = newAddress;
          sid = selenium.getCookieByName("JSESSIONID").getValue();
       }
-      else 
+      else
       {
-         //get sessionid directly from browser URL if JSESSIONID cookie is not present
+         // get sessionid directly from browser URL if JSESSIONID cookie is not present
          firstPart = newAddress.substring(0, newAddress.indexOf(";"));
          sid = loc.substring(loc.indexOf("jsessionid=") + "jsessionid=".length(), loc.length());
       }
-      
+
       newAddress = firstPart + ";jsessionid=" + sid;
-      
+
       return newAddress;
    }
-      
+
    public void shutdownMasterJBossInstance()
    {
-      String command = jboss + "bin/shutdown.sh -s localhost:1099 -S";
-      System.out.println("[DEBUG] " + command);
+      String command = jbossConfig + "/../../bin/shutdown.sh -s service:jmx:rmi:///jndi/rmi://localhost:1090/jmxrmi -S";
       try
       {
          Process process = Runtime.getRuntime().exec(command);
@@ -271,30 +265,29 @@ public class NumberGuessClusteringTest extends AbstractTestCase
       {
       }
    }
-   
-   public void initializeSecondBrowser() {
+
+   public void initializeSecondBrowser()
+   {
+      System.out.println("localContextPath: " + localContextPath);
       selenium = new AjaxSeleniumImpl(getSeleniumHost(), getSeleniumPort(), browser, buildUrl(SECOND_INSTANCE_BROWSER_URL, localContextPath));
       AjaxSeleniumProxy.setCurrentContext(selenium);
 
-      selenium.enableNetworkTrafficCapturing(isSeleniumNetworkTrafficEnabled());
+      selenium.enableNetworkTrafficCapturing(true);
       selenium.start();
-     
+      selenium.deleteAllVisibleCookies();
       loadCustomLocationStrategies();
-     
       selenium.setSpeed(getSeleniumSpeed());
-     
-      if (isSeleniumMaximize()) {
+
+      if (isSeleniumMaximize())
+      {
          // focus and maximaze tested window
          selenium.windowFocus();
          selenium.windowMaximize();
       }
    }
-   
-   /**
-     * Uses selenium.addLocationStrategy to implement own strategies to locate items in the tested page
-     */
-   private void loadCustomLocationStrategies() {
-      // jQuery location strategy
+
+   private void loadCustomLocationStrategies()
+   {
       JavaScript strategySource = fromResource("javascript/selenium-location-strategies/jquery-location-strategy.js");
       selenium.addLocationStrategy(ElementLocationStrategy.JQUERY, strategySource);
    }
